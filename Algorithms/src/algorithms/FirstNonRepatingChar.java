d8c8a1c433311bfb7ba4b9f6ec51d0f1e41aed49
Mathew Wright <40322524+Mathew-Wright@users.noreply.github.com>
2019-07-24 12:19:37 +0100

    Implemented FirstNonRepeatingChar (added comments for LinkedHashMap)

--- Algorithms/src/algorithms/FirstNonRepatingChar.java
+++ Algorithms/src/algorithms/FirstNonRepatingChar.java
@@ -9,41 +9,33 @@ import java.util.*;
 import java.io.*;
 
 /**
- * Given a string S consisting of lowercase Latin Letters. Find the first non repeating character in S
+ * Given a string S consisting of lowercase Latin Letters. Find the first non
+ * repeating character in S
  * https://practice.geeksforgeeks.org/problems/non-repeating-character/0
+ *
  * @author G4G template
  */
 public class FirstNonRepatingChar {
 
     /**
-     * Reads formulated console input for the input string s, prints out the first char in s
-     * that only occurred once in the string s.
-     * 
-     * Input:
-     * The first line contains T denoting the number of test cases. Then follows description of test cases.
-     * Each case begins with a single integer N denoting the length of string. The next line contains the string S.
-     * 
-     * Output:
-     * For each test case, print the first non repeating character present in string. Print -1 if there is no non repeating character.
-     * 
-     * 
-     * 
-     * Example:
-     * Input : 
-     * 3
-     * 5  
-     * hello
-     * 12
-     * zxvczbtxyzvy
-     * 6
-     * xxyyzz
-     * 
-     * Output :
-     * h
-     * c
-     * -1
+     * Reads formulated console input for the input string s, prints out the
+     * first char in s that only occurred once in the string s.
+     *
+     * Input: The first line contains T denoting the number of test cases. Then
+     * follows description of test cases. Each case begins with a single integer
+     * N denoting the length of string. The next line contains the string S.
+     *
+     * Output: For each test case, print the first non repeating character
+     * present in string. Print -1 if there is no non repeating character.
+     *
+     *
+     *
+     * Example: Input : 3 5 hello 12 zxvczbtxyzvy 6 xxyyzz
+     *
+     * Output : h c -1
+     *
      * @param args
-     * @throws IOException 
+     * @throws IOException
      */
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
@@ -55,35 +47,54 @@ public class FirstNonRepatingChar {
             StringBuilder sb = new StringBuilder();
             String str = br.readLine().trim().split(" ")[0];
 
-            System.out.println("output: "+firstNonRepeatingChar(str));
+            System.out.println("output: " + firstNonRepeatingChar(str));
 
             t--;
         }
     }
 
     /**
-     * Handler method, given a string str, returns the first char in str that 
+     * Handler method, given a string str, returns the first char in str that
      * only occurred once in str.
+     *
      * @param str string to search
-     * @return first char in s that  only occurred once in str
+     * @return first char in s that only occurred once in str
      */
-    private static String firstNonRepeatingChar(String str){
-	    HashMap<String, Integer> freq = new HashMap<>();
-	    for (int i = 0; i < str.length(); i++){
-	        String s = String.valueOf(str.charAt(i));
-	        freq.put(s,freq.containsKey(s) ? 2 : 1);
-	        
-	        
-	        
-	        // value 2 = repeating
-	        // value 1 = unique
-	    }
-	    
-	    for (int i = 0; i < str.length(); i++){
-	        if (freq.get(String.valueOf(str.charAt(i))) == 1){
-	            return String.valueOf(str.charAt(i));
-	        }
-	    }
-	    return "-1";
-	}
+    private static String firstNonRepeatingChar(String str) {
+        HashMap<String, Integer> freq = new HashMap<>();
+        for (int i = 0; i < str.length(); i++) {
+            String s = String.valueOf(str.charAt(i));
+            freq.put(s, freq.containsKey(s) ? 2 : 1);
+            // value 2 = repeating
+            // value 1 = unique
+        }
+
+        for (int i = 0; i < str.length(); i++) {
+            if (freq.get(String.valueOf(str.charAt(i))) == 1) {
+                return String.valueOf(str.charAt(i));
+            }
+        }
+        return "-1";
+        
+        //LHM impementation
+        //LinkedHashMap<String,Integer> l=new LinkedHashMap<>();
+	//HashMap<String, Integer> rejected = new HashMap<>();
+
+        //for(int i=0;i<str.length();i++) {
+        //    String s = String.valueOf(str.charAt(i));
+        //    
+        //    if (l.containsKey(s)){
+        //        l.remove(s);
+        //        rejected.put(s,1);
+        //    }
+        //    if (!rejected.containsKey(s)){
+        //        l.put(s,1);    
+        //    }
+        //}
+        //
+        //if (l.size() == 0){
+        //    return "-1";
+        //}
+        //return l.keySet().iterator().next();
+    }
 }
