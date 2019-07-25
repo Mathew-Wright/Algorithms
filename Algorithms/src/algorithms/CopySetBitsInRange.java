46b6a96ee364976aa9e6d77e146fb121080852f0
Mathew Wright <40322524+Mathew-Wright@users.noreply.github.com>
2019-07-25 10:14:01 +0100

    Implemented CopySetBitsInRange

---
+++ Algorithms/src/algorithms/CopySetBitsInRange.java
@@ -0,0 +1,75 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package algorithms;
+
+import java.util.*;
+import java.io.*;
+
+/**
+ * Given two numbers x and y, and a range [l, r] ,
+ * For each set bits of y in range [l, r], set these bits in x also.
+ * https://practice.geeksforgeeks.org/problems/copy-set-bits-in-range/0
+ * @author Mathew Wright
+ */
+public class CopySetBitsInRange {
+
+    /**
+     * Reads formulated console input for x,lsb,msb,y values.
+     * 
+     * x = int to copy set bits into
+     * lsb = least significant bit to start from
+     * msb = most significant bit to end on
+     * y = int to copy bits from
+     * 
+     * 
+     * Input:
+     * The first line of input contains an integer T denoting the number of test cases. 
+     * Each test case contains space separated values of x, lsb, msb and y in a single line.
+     * 
+     * Output:
+     * Print modified value of x.
+     * 
+     * Example:
+     * 
+     * Input:
+     * 2
+     * 44 1 5 3
+     * 16 1 3 2
+     * 
+     * Output:
+     * 47
+     * 18
+     * @param args
+     * @throws IOException 
+     */
+    public static void main(String[] args) throws IOException {
+        Scanner scanner = new Scanner(System.in);
+        int t = scanner.nextInt();
+        while (t > 0) {
+
+            int x = scanner.nextInt();
+            int lsb = scanner.nextInt();
+            int msb = scanner.nextInt();
+            int y = scanner.nextInt();
+
+            System.out.println("output: "+copySetBitsInRange(x, lsb, msb, y));
+
+            t--;
+        }
+    }
+
+    private static int copySetBitsInRange(int x, int lsb, int msb, int y) {
+
+        for (int i = lsb - 1; i < msb + 1; i++) {
+
+            if ((y & (1 << i)) != 0) {//if y bit is set at i
+                x = x | (1 << i);
+            }
+        }
+        return x;
+
+    }
+}
